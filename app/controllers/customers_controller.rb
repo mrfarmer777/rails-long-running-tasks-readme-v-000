170bb6a8fc7877d2d95db4db892e88ef7c700a4f
Matt Farmer <mrfarmer777@gmail.com>
2018-07-30 21:15:40 +0000

    automatically backed up by learn

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,16 @@
+require 'csv'
+
 class CustomersController < ApplicationController
 
   def index
     @customers = Customer.all
+  end
+  
+  def upload #the upload action
+    CSV.foreach(params[:leads].path, headers: true) do |lead|   #for each row in the csv file (found with params[leads]path)
+      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])  #make a new customer object
+    end
+    redirect_to customers_path
   end
 
 end
